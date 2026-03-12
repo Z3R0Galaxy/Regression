7b63cad588cf2ae244ca4124441342add62b481d
Z3R0Galaxy <orlandogien@icloud.com>
2026-03-12 14:49:25 +1100

    added f strings to the print

--- linear.py
+++ linear.py
@@ -8,10 +8,12 @@ y = np.array([1, 3, 5, 7, 9, 11, 13, 15])
 model = LinearRegression()
 model.fit(x, y)
 
-print(model.predict([[68]])) 
-print(f"y = {model.coef_[0]:.2f}x + {model.intercept_:.2f}")
+prediction = model.predict([[68]])[0]
 
-plt.scatter(x,y)
-plt.plot(x,model.predict(x))
+print(f"prediction: {prediction:.2f}")
+print(f"equation: y = {model.coef_[0]:.2f}x + {model.intercept_:.2f}")
+
+plt.scatter(x, y)
+plt.plot(x, model.predict(x))
 plt.title("Linear Regression")
 plt.show()
